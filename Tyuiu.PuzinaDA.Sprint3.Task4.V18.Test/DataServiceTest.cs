377bda777657a0868bc9b3aa3c8b72ae692db3d8
Tyuiu.puzinada@outlook.com <dariapuzina7b@gmail.com>
2024-09-29 20:50:36 +0500

    Task4

---
+++ Tyuiu.PuzinaDA.Sprint3.Task4.V18.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.PuzinaDA.Sprint3.Task4.V18.Lib;
+namespace Tyuiu.PuzinaDA.Sprint3.Task4.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Checked()
+        {
+            DataService ds = new DataService();
+            int startValue = -5, stopValue = 5;
+            double res = ds.Calculate(startValue, stopValue);
+            double wait = 2.993;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
